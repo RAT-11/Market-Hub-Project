7dfa186720954431616a29d44c06e83de12495e1
Rudra Pati <rudrapati0204@gmail.com>
2025-07-17 20:17:46 +0530

    Backup: current version before folder restructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
 
 
 
-import React from 'react';
+// import React from 'react';
+// import Navbar from './components/Navbar';
+// import HeroSection from './components/HeroSection';
+// import FeaturesSection from './components/FeaturesSection';
+// import UserChoiceSection from './components/UserChoiceSection';
+// import BuyerLogin from "./components/BuyerLogin";
+// import SellerLogin from './components/SellerLogin';
+// import HowItWorksSection from './components/HowItWorksSection';
+// import Footer from './components/Footer';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import './App.css';
+
+
+// function LandingPage() {
+//   return (
+//     <>
+//       <Navbar />
+//       <HeroSection />
+//       <FeaturesSection />
+//       <UserChoiceSection />
+//       <HowItWorksSection />
+//       <div className="demo-cta">
+//         <h3>Still questions ?</h3>
+//         <button className="demo-btn">
+//           Request a Demo. <span className="arrow">↗</span>
+//         </button>
+//       </div>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Routes>
+//           <Route path="/" element={<LandingPage />} />
+//           <Route path="/seller-login" element={<SellerLogin />} />
+//           <Route path="/buyer-login" element={<BuyerLogin />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+// ----------------------------------------------------------------------------------------------------
+
+
+
+
+import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import FeaturesSection from './components/FeaturesSection';
@@ -12,7 +65,8 @@ import HowItWorksSection from './components/HowItWorksSection';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function LandingPage() {
   return (
@@ -22,7 +76,7 @@ function LandingPage() {
       <FeaturesSection />
       <UserChoiceSection />
       <HowItWorksSection />
-      <div className="demo-cta">
+      <div className="demo-cta" data-aos="fade-up">
         <h3>Still questions ?</h3>
         <button className="demo-btn">
           Request a Demo. <span className="arrow">↗</span>
@@ -34,6 +88,13 @@ function LandingPage() {
 }
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
   return (
     <Router>
       <div className="App">
@@ -48,4 +109,3 @@ function App() {
 }
 
 export default App;
-
